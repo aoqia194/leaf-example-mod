cd52f87669a313a5f68ffbcd06c9e94f937eca70
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2025-04-09 14:34:04 +1000

    feat: Add vanilla logger implementation usage
    feat: Add vanilla logger implementation usage

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,11 +2,15 @@ package com.example;
 
 import dev.aoqia.leaf.api.ModInitializer;
 
+import zombie.debug.DebugLog;
+import zombie.debug.DebugLogStream;
+
 public class ExampleMod implements ModInitializer {
     public static final String MOD_ID = "modid";
+    public static final DebugLogStream LOGGER = DebugLog.Mod;
 
     @Override
     public void onInitialize() {
-        System.out.println("Hello Leaf World!!! >w<");
+        LOGGER.debugln("[%s] %s", MOD_ID, "Hello Leaf World!!! >w<");
     }
 }
--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -4,15 +4,25 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import zombie.Lua.LuaEventManager;
+import zombie.network.GameServer;
+
+import static com.example.ExampleMod.LOGGER;
 
 @Mixin(LuaEventManager.class)
 public class ExampleMixin {
     @Inject(method = "triggerEvent", at = @At("HEAD"))
     private static void triggerEvent(String event, CallbackInfo ci) {
+        // Only capture events if executed on the dedicated server.
+        // if (!GameServer.bServer) {
+        //     return;
+        // }
+
+        // Filter out spammy events.
         if (!event.equals("OnRenderTick") && !event.equals("OnPreUIDraw") &&
-            !event.equals("OnPostUIDraw")) {
-            System.out.printf("ExampleMixin -> triggerEvent(%s) called.%n", event);
+            !event.equals("OnPostUIDraw") && !event.equals("OnPostRender")) {
+            LOGGER.println("[ExampleMixin] triggerEvent called with args (%s).", event);
         }
     }
 }
